c1a994cce1976825ba41fff28c50400e6b2e9a80
Alessandro Pettenuzzo <alessandro.pettenuzzo96@gmail.com>
2023-05-20 15:36:09 +0200

    Update abbreviations.py

--- TTS/tts/utils/text/spanish/abbreviations.py
+++ TTS/tts/utils/text/spanish/abbreviations.py
@@ -1,26 +1,16 @@
 import re
 
-# List of (regular expression, replacement) pairs for abbreviations in english:
+# List of (regular expression, replacement) pairs for abbreviations in spanish:
 abbreviations_es = [
     (re.compile("\\b%s\\." % x[0], re.IGNORECASE), x[1])
     for x in [
-        ("mrs", "misess"),
-        ("mr", "mister"),
+        ("sr", "señor"),
+        ("sra", "señora"),
+        ("srita", "señorita"),
+        ("ud", "usted"),
+        ("ej", "ejemplo"),
+        ("etc", "etcetera"),
         ("dr", "doctor"),
-        ("st", "saint"),
-        ("co", "company"),
-        ("jr", "junior"),
-        ("maj", "major"),
-        ("gen", "general"),
-        ("drs", "doctors"),
-        ("rev", "reverend"),
-        ("lt", "lieutenant"),
-        ("hon", "honorable"),
-        ("sgt", "sergeant"),
-        ("capt", "captain"),
-        ("esq", "esquire"),
-        ("ltd", "limited"),
-        ("col", "colonel"),
-        ("ft", "fort"),
+        ("st", "santo"),
     ]
 ]
